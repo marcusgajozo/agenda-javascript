fe783e0a57c2bf4fe7ce51766527205f963429d5
Marcus Gajozo <marcusgajozo@gmail.com>
2023-05-20 15:58:17 -0400

    criação do loginModel para registrar usuário no sistema

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,14 @@
-
+const Login = require("../models/loginModel");
 exports.index = (req, res) => {
-    res.render('login');
-}
+  res.render("login");
+};
+
+exports.register = async (req, res) => {
+  const login = new Login(req.body);
+  await login.register();
+
+  if (login.errors.length > 0) {
+    // colocar flash messages
+  }
+  res.send(login.body);
+};
