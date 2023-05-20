8e41ac99c1895a0f2d2d2ee9a4f0e2ba06f3b02a
Marcus Gajozo <marcusgajozo@gmail.com>
2023-05-19 20:27:47 -0400

    criação da página de login e cadastro, criação de controller para login e cadastro, atualização de rotas

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -8,9 +8,11 @@ exports.outroMiddleware = (req, res, next) => {
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err &&  err.code === 'EBADCSRFTOKEN'){
-        return res.send('Ban CSRF. ');
+    if(err){
+        return res.render('404');
     }
+
+    next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
